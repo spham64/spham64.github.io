ccb8e52e29bdc61eaf76f16e703ac64ee1153f77
spham64 <samuelpham64@gmail.com>
2025-08-26 18:33:52 -0400

    React Router Setup

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Samuel Pham</h1>
-      <div class="card">
-        <p>
-          I am a student at UNCC from Charlotte, NC, with a passion for solving
-          challenging problems using cutting-edge technologies and tools. This
-          passion has led me into the software development field, where I have
-          researched and worked on ways to support literacy and education for
-          all.
-        </p>
-        <p>
-          When I'm not coding or studying, I enjoy activites that keep my
-          physically and mentally active. Whether it's going to the gym,
-          reading, or staying up-to-date with the latest trends in technology
-          and economics, I’m always looking for ways to stay engaged and
-          continue learning.
-        </p>
-      </div>
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <>
+      <h1>Samuel Pham</h1>
+      <div class="card">
+        <p>
+          I am a student at UNCC from Charlotte, NC, with a passion for solving
+          challenging problems using cutting-edge technologies and tools. This
+          passion has led me into the software development field, where I have
+          researched and worked on ways to support literacy and education for
+          all.
+        </p>
+        <p>
+          When I'm not coding or studying, I enjoy activites that keep my
+          physically and mentally active. Whether it's going to the gym,
+          reading, or staying up-to-date with the latest trends in technology
+          and economics, I’m always looking for ways to stay engaged and
+          continue learning.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
